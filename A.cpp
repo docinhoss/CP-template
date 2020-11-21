d07f5d22b07e32ba511206f6be725e4adfedbe3c
docinhoss <73991168+docinhoss@users.noreply.github.com>
2020-11-21 23:49:30 +0000

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define trav(a,x) for(auto& a : x)
+#define rep(i,a,b) for(int i = a; i < (b); ++i)
+#define size(s) (int)(x).size()
+
+#define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
+#define UP(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
+#define RDUPLICATE(s) s.erase(unique(s.begin(), s.end()), s.end()) 
+
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  
+
+  return 0;
+}
