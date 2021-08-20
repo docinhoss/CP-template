9662bd1077ae64f08af53dfff38aa7bb16dce1dd
JPMM <73991168+docinhoss@users.noreply.github.com>
2021-08-20 11:56:53 +0100

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,10 +3,15 @@
 #define trav(a,x) for(auto& a : x)
 #define rep(i,a,b) for(int i = a; i < (b); ++i)
 #define sz(v) ((int)((v).size()))
+#define all(x) begin(x), end(x)
 
 #define LOWER(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define UP(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
-#define RDUPLICATE(s) s.erase(unique(s.begin(), s.end()), s.end()) 
+#define RDUPLICATE(s) s.erase(unique(s.begin(), s.end()), s.end())
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
 
 using namespace std;
 
